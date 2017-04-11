d53900ddd6d0747bef9df8838f8e4b0710c1e0eb
Chris Jordan <cjorda15@msudenver.edu>
2017-04-10 22:17:27 -0600

    added location component and some more css

--- src/CurrentTemp.js
+++ src/CurrentTemp.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react'
-
+import Location from "./Location.js"
 class CurrentTemp extends Component {
   render() {
 
     return (
       <div className="current-temp">
+        <Location/>
+        <div className="current-temp-content">
         <h2>Today:</h2>
         <h3>70/35</h3>
+        </div>
       </div>
 
     )
---
+++ src/Location.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+
+class Location extends Component{
+  render(){
+    return(
+      <div className="current-location-container">
+      <div className ="city">Denver,</div>
+      <div className ="State">Colorado</div>
+      </div>
+    )
+  }
+}
+
+
+export default Location
