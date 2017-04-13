8feca435a618f341fdb3a81fc99beaf24daa959d
Chris Jordan <cjorda15@msudenver.edu>
2017-04-13 15:03:13 -0600

    got days onto list

--- src/Card.js
+++ src/Card.js
@@ -16,24 +16,41 @@ class Card extends Component {
 
   }
 
+  renderType(){
+    if(this.props.type ==="hour"){
+      return (
+        <article className="card">
 
+          <h6>{this.props.info.FCTTIME.civil}</h6>
+          <img src={this.props.info.icon_url} />
+          <h6>{this.props.info.temp.english} degrees </h6>
+        </article>
+
+      )
+    }else{
+
+
+      return (
+        <article className="card">
+
+          <h6>{this.props.info.FCTTIME.weekday_name}</h6>
+          <img src={this.props.info.icon_url} />
+          <h6>{this.props.info.temp.english} degrees </h6>
+        </article>
+
+      )
+
+
+    }
+
+  }
 
 
   render() {
 
     // if(this.props.type)
     return (
-
-
-
-
-
-      <article className="card">
-
-        <h6>{this.props.info.FCTTIME.civil}</h6>
-        <img src={this.props.info.icon_url} />
-        <h6>{this.props.info.temp.english} degrees </h6>
-      </article>
+this.renderType()
     )
   }
 }
