8f6b5b73fd5c3c0f22d9680416a6e81a1f0f07c4
Devon Bull <dbull@live.com>
2017-04-14 18:41:09 -0600

    map generating new cards on click, need to pass unique data

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,34 @@ import React, {Component} from 'react'
 
 class Card extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      clicked: false
+    }
+  }
+
+  test() {
+    let change = !this.state.clicked
+    this.setState({clicked: change})
+  }
+
+  test2() {
+    if(this.state.clicked) {
+      let dumb = [1,2,3,4,5,6,7,8,9,10]
+      return (
+        dumb.map(dum => {
+          return (
+            <article className="hourlyCards">
+              <h6>{this.props.info.FCTTIME.civil}</h6>
+              <img src={this.props.info.icon_url} />
+              <h6>{this.props.info.temp.english} degrees </h6>
+            </article>
+          )
+        })
+      )
+    }
+  }
 
   renderType(){
     if(this.props.type ==="hour"){
@@ -15,12 +43,15 @@ class Card extends Component {
       )
     }else{
       return (
-        <article className="card">
+        <div>
+          <article className="card" onClick={() => {this.test()}}>
 
-          <h6>{this.props.info.FCTTIME.weekday_name}</h6>
-          <img src={this.props.info.icon_url} />
-          <h6>{this.props.info.temp.english} degrees </h6>
-        </article>
+            <h6>{this.props.info.FCTTIME.weekday_name}</h6>
+            <img src={this.props.info.icon_url} />
+            <h6>{this.props.info.temp.english} degrees </h6>
+          </article>
+          {this.test2()}
+        </div>
 
       )
     }
