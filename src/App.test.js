88f4e75ba66db2fadc82503832cbf5010f10c3c1
Devon Bull <dbull@live.com>
2017-04-15 13:53:09 -0600

    Crazy jest testing setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import $ from 'jquery'
+import {shallow,mount,render} from 'enzyme'
+var expect = require("chai").expect
+
+const wrapper = shallow(<App/>)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
 });
