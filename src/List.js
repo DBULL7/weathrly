f70cfb821b0a7a410cd1364990b2f41a79c1fc54
Chris Jordan <cjorda15@msudenver.edu>
2017-04-15 18:31:09 -0600

    after nate reviw

--- src/List.js
+++ src/List.js
@@ -11,28 +11,32 @@ class List extends Component {
       <section className="list">
         <ListTitle text={this.props.title}/>
         {
-        this.props.listItems.map(function(value,index,array){
+          // loop through listItems, each 24th list item should have a hours property which is an array of hours
+
+
+        this.props.listItems.map(function(value, index, array){
+          // value.type = "daily";
         if(index===10){
           return
         }
 
-        if(array.length===11){
-             return (
-             <Card
-             index={index}
-              type ="daily"
-              key={index}
-              info={value}
-            />)
+        if (array.length===11){
+          return (
+            <Card
+                index={index}
+                type ="daily"
+                key={index}
+                info={value} />
+          )
         }
 
-        return <Card
-          
-          index={index}
-          type="hour"
-          key={index}
-          info={value}
-         />
+        return (
+          <Card
+              index={index}
+              type="hour"
+              key={index}
+              info={value} />
+        )
       })
 
       }
